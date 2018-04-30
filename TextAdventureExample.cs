6668e3540db37074aaf19890db98add7e148dbd5
kutterback <kutterback@gradleaders.com>
2018-04-30 12:04:34 -0400

    cleaning up code base

---
+++ TextAdventureExample.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using TextInterpreter.Common;
+
+namespace TextInterpreter
+{
+    class Program
+    {
+        static BaseState GameState;
+        static Screen Screen;
+        static GameManager GameManager;
+        static void Main(string[] args)
+        {
+            StartSequence();
+            GameLoop();
+        }
+        static void StartSequence()
+        {
+            GameState = new BaseState();
+            //Set saved game state here
+            GameManager = new GameManager(GameState);
+            GameState.Set(CommonEnums.Controls.None);
+            List<string> start = new List<string>() { "start" };
+            Screen = new Screen(GameManager.Process(start));
+            Screen.Render();
+        }
+        static void GameLoop()
+        {
+            if (GameState.Get == CommonEnums.Controls.None)
+            {
+                Screen.ReadToBuffer();
+                Screen.WriteToBuffer(GameManager.Process(Screen.Input));
+                if (GameState.Get == CommonEnums.Controls.Exit)
+                {
+                    GameState.Increment();
+                    //Save state here
+                }
+                else if (GameState.Get == CommonEnums.Controls.Clear)
+                {
+                    Screen.Clear();
+                    GameState.Set(CommonEnums.Controls.None);
+                    GameState.Increment();
+                    GameLoop();
+                }
+                else
+                {
+                    Screen.Render();
+                    GameState.Increment();
+                    GameLoop();
+                }
+            }
+            else if(GameState.Get == CommonEnums.Controls.Dialog)
+            {
+                Screen.ReadToBuffer();
+                string response = GameManager.ContinueDialog(Screen.Input);
+                if(response == null)
+                {
+                    GameState.Set(CommonEnums.Controls.None);
+                    List<string> End = new List<string>();
+                    End.Add("c");
+                    Screen.WriteToBuffer(GameManager.ContinueDialog(End));
+                }
+                else
+                {
+                    Screen.WriteToBuffer(response);
+                }
+                if (GameState.Get == CommonEnums.Controls.Clear)
+                {
+                    Screen.Clear();
+                    GameState.Set(CommonEnums.Controls.Dialog);
+                    GameState.Increment();
+                    GameLoop();
+                }
+                else
+                {
+                    Screen.Render();
+                    GameState.Increment();
+                    GameLoop();
+                }
+            }
+            Screen.Exit();
+        }
+    }
+}
