35a821f46d1ff686fa8d6cdf15a499f7f15647d1
kutterback <kutterback@gradleaders.com>
2018-04-30 12:13:16 -0400

    Revert "clean remote"
    This reverts commit 474a6a41aa70066a7764de6a849d46992a046ee9.

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TextInterpreter.GameObjects;
+using TextInterpreter.Common;
+
+namespace TextInterpreter
+{
+    public class Player : BaseCharacter
+    {
+        InventoryManager Inventory;
+        public Player(CommonEnums.LocationType initialLocation)
+        {
+            Inventory = new InventoryManager();
+            SetLocation(initialLocation);
+        }
+        public string Take(CommonEnums.Interactables item)
+        {
+            string response;
+            Inventory.AddItemToInventory(item, out response);
+            if(response == null)
+            {
+                response = item.ToString() + " has been added to your inventory.";
+            }
+            return response;
+        }
+        public string Drop(CommonEnums.Interactables item)
+        {
+            Inventory.RemoveItemFromInventory(item, out string response);
+            return response;
+        }
+        public string PlayerInventory()
+        {
+            return Inventory.GetInventory();
+        }
+    }
+}
